030a9e4cd77d2d6257fc4580dc46d573f496db40
YeJeeO <yejeeo@yandex.ru>
2023-07-20 22:46:54 +0700

    modified:   components/coach.js 	renamed:    public/1677640111_bogatyr-club-p-studiya-tantsev-foni-instagram-6.jpg -> public/16776401116.jpg 	renamed:    public/1677640172_bogatyr-club-p-studiya-tantsev-foni-instagram-75.jpg -> public/16776475.jpg

--- components/coach.js
+++ components/coach.js
@@ -1,8 +1,63 @@
+import { Image, Grid} from "@nextui-org/react";
+
 const Coach = () => {
     return (
       <div>
         <h1>Наши педагоги</h1>
+        <Grid.Container gap={2}>
+       <Grid>
+        <Image
+          src="16776401116.jpg"
+          alt="A"
+          width={100}
+          height={100}
+        />
+      </Grid> 
+      <Grid>
+        <Image
+          src="16776401116.jpg"
+          alt="A"
+          width={200}
+          height={350}
+        />
+      </Grid>
+      <Grid>
+        <Image
+          src="16776401116.jpg"
+          alt="A"
+          width={400}
+          height={350}
+        />
+      </Grid>
+    </Grid.Container>
+    <Grid.Container gap={1}>
+       <Grid>
+        <Image
+          src="16776475.jpg"
+          alt="A"
+          width={100}
+          height={100}
+        />
+      </Grid> 
+      <Grid>
+        <Image
+          src="16776475.jpg"
+          alt="A"
+          width={150}
+          height={150}
+        />
+      </Grid>
+      <Grid>
+        <Image
+          src="16776475.jpg"
+          alt="A"
+          width={300}
+          height={200}
+        />
+      </Grid>
+    </Grid.Container>
       </div>
+      
     );
   };
    
