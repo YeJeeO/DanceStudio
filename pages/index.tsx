93a1b4d687e7bb278053d91c65c6d722091b073c
YeJeeO <yejeeo@yandex.ru>
2023-07-23 16:16:06 +0700

    First page 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.tsx 	modified:   pnpm-lock.yaml

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import First from './first.js'
+import Second from './second.js'
+import Third from './third.js'
+import Fouth from './fouth.js'
+import Fifth from './fifth.js'
 
 export default function Home() {
 		return <>
         <First/>
+		<Second/>
+		<Third/>
+		<Fouth/>
+		<Fifth/>
 		</>;
 	}
